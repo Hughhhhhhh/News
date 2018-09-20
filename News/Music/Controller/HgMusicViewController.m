1ca4448339a9a5d80ef2b34ad1e4a20187ca08d1
czg <834244439@qq.com>
2018-09-20 11:08:36 +0800

    优化锁屏信息

--- News/Music/Controller/HgMusicViewController.m
+++ News/Music/Controller/HgMusicViewController.m
@@ -21,6 +21,10 @@
 
 @property (nonatomic, strong) id playerTimeObserver;
 
+@property (nonatomic, strong) UIImageView * lrcImageView;
+
+@property (nonatomic, strong) UIImage * lastImage;//最后一次锁屏之后的歌词海报
+
 @end
 
 @implementation HgMusicViewController
@@ -32,6 +36,8 @@
      |NSKeyValueObservingOptionNew context:nil];
     [HgSongInfo.shared addObserver:self forKeyPath:@"isDataRequestFinish" options:NSKeyValueObservingOptionOld
      |NSKeyValueObservingOptionNew context:nil];
+    // 锁屏播放设置
+    [self createRemoteCommandCenter];
 }
 
 #pragma mark - KVO
@@ -51,13 +57,13 @@
 }
 
 -(void)playSongSetting{
-//    dispatch_async(dispatch_get_main_queue(), ^{
-//
-//        [self.detailController.midView.midLrcView.tableView scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0] atScrollPosition:UITableViewScrollPositionMiddle animated:YES];
-//
-//    });
-//
-//    [self.detailController.midView.midLrcView.tableView reloadData];
+    dispatch_async(dispatch_get_main_queue(), ^{
+
+        [self.detailController.midView.midLrcView.tableView scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0] atScrollPosition:UITableViewScrollPositionMiddle animated:YES];
+
+    });
+
+    [self.detailController.midView.midLrcView.tableView reloadData];
     
     if (self.playerTimeObserver != nil) {
         
@@ -142,7 +148,131 @@
                 
             }
         }
+        //监听锁屏状态 lock=1则为锁屏状态
+        uint64_t locked;
+        __block int token = 0;
+        notify_register_dispatch("com.apple.springboard.lockstate",&token,dispatch_get_main_queue(),^(int t){
+        });
+        notify_get_state(token, &locked);
+        
+        //监听屏幕点亮状态 screenLight = 1则为变暗关闭状态
+        uint64_t screenLight;
+        __block int lightToken = 0;
+        notify_register_dispatch("com.apple.springboard.hasBlankedScreen",&lightToken,dispatch_get_main_queue(),^(int t){
+        });
+        notify_get_state(lightToken, &screenLight);
+        
+        BOOL isShowLyricsPoster = NO;
+        // NSLog(@"screenLight=%llu locked=%llu",screenLight,locked);
+        if (screenLight == 0 && locked == 1) {
+            //点亮且锁屏时
+            isShowLyricsPoster = YES;
+        }else if(screenLight){
+            return;
+        }
+        
+        //展示锁屏歌曲信息，上面监听屏幕锁屏和点亮状态的目的是为了提高效率
+        [weakSelf showLockScreenTotaltime:totalTime andCurrentTime:currentTime andLyricsPoster:isShowLyricsPoster];
     }];
+    
+}
+
+#pragma mark - 锁屏播放设置
+//展示锁屏歌曲信息：图片、歌词、进度、演唱者
+- (void)showLockScreenTotaltime:(float)totalTime andCurrentTime:(float)currentTime andLyricsPoster:(BOOL)isShow{
+    
+        NSMutableDictionary * songDict = [[NSMutableDictionary alloc] init];
+        //设置歌曲题目
+        [songDict setObject:HgSongInfo.shared.title forKey:MPMediaItemPropertyTitle];
+        //设置歌手名
+        [songDict setObject:HgSongInfo.shared.author forKey:MPMediaItemPropertyArtist];
+        //设置专辑名
+        [songDict setObject:HgSongInfo.shared.album_title forKey:MPMediaItemPropertyAlbumTitle];
+        //设置歌曲时长
+        [songDict setObject:[NSNumber numberWithDouble:totalTime]  forKey:MPMediaItemPropertyPlaybackDuration];
+        //设置已经播放时长
+        [songDict setObject:[NSNumber numberWithDouble:currentTime] forKey:MPNowPlayingInfoPropertyElapsedPlaybackTime];
+    
+        NSString * lrcImage = HgSongInfo.shared.pic_big;
+        [[SDWebImageDownloader sharedDownloader] downloadImageWithURL:[NSURL URLWithString:lrcImage] options:0 progress:nil completed:^(UIImage * _Nullable image, NSData * _Nullable data, NSError * _Nullable error, BOOL finished) {
+            if (image) {
+                if (isShow) {
+                    
+                    //制作带歌词的海报
+                    if (!_lrcImageView) {
+                        _lrcImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 480,800)];
+                    }
+                    
+                    
+                    //主要为了把歌词绘制到图片上，已达到更新歌词的目的
+                    _lrcImageView.image = image;
+                    _lrcImageView.backgroundColor = [UIColor blackColor];
+                    
+                    //获取添加了歌词数据的海报图片
+                    UIGraphicsBeginImageContextWithOptions(_lrcImageView.frame.size, NO, 0.0);
+                    CGContextRef context = UIGraphicsGetCurrentContext();
+                    [_lrcImageView.layer renderInContext:context];
+                    _lrcImageView.image = UIGraphicsGetImageFromCurrentImageContext();
+                    _lastImage = image;
+                    UIGraphicsEndImageContext();
+                    
+                }else{
+                    if (_lastImage) {
+                        _lrcImageView.image = _lastImage;
+                    }
+                }
+                //设置显示的海报图片
+                [songDict setObject:[[MPMediaItemArtwork alloc] initWithImage:image]
+                             forKey:MPMediaItemPropertyArtwork];
+                
+            }
+            [[MPNowPlayingInfoCenter defaultCenter] setNowPlayingInfo:songDict];
+        }];
+}
+
+#pragma mark - 锁屏界面开启和监控远程控制事件
+//锁屏界面开启和监控远程控制事件
+- (void)createRemoteCommandCenter{
+    
+    // 远程控制命令中心 iOS 7.1 之后  详情看官方文档：https://developer.apple.com/documentation/mediaplayer/mpremotecommandcenter
+    
+    MPRemoteCommandCenter *commandCenter = [MPRemoteCommandCenter sharedCommandCenter];
+    
+    // 远程控制播放
+    [commandCenter.pauseCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
+        [HgMusicPlayerManager.shared.play pause];
+        return MPRemoteCommandHandlerStatusSuccess;
+    }];
+    
+    // 远程控制暂停
+    [commandCenter.playCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
+        [HgMusicPlayerManager.shared.play play];
+        return MPRemoteCommandHandlerStatusSuccess;
+    }];
+    
+    // 远程控制上一曲
+    [commandCenter.previousTrackCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
+        NSLog(@"上一曲");
+        return MPRemoteCommandHandlerStatusSuccess;
+    }];
+    
+    // 远程控制下一曲
+    [commandCenter.nextTrackCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
+        NSLog(@"下一曲");
+        [self nextButtonAction:nil];
+        return MPRemoteCommandHandlerStatusSuccess;
+    }];
+    
+    
+    //在控制台拖动进度条调节进度（仿QQ音乐的效果）
+    [commandCenter.changePlaybackPositionCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
+        CMTime totlaTime = HgMusicPlayerManager.shared.play.currentItem.duration;
+        MPChangePlaybackPositionCommandEvent * playbackPositionEvent = (MPChangePlaybackPositionCommandEvent *)event;
+        [HgMusicPlayerManager.shared.play seekToTime:CMTimeMake(totlaTime.value*playbackPositionEvent.positionTime/CMTimeGetSeconds(totlaTime), totlaTime.timescale) completionHandler:^(BOOL finished) {
+        }];
+        return MPRemoteCommandHandlerStatusSuccess;
+    }];
+    
     
 }
 
